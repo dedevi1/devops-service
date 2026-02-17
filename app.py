5b5fd7f254066fe0128f838a6a8f3f549114f208
dedevi1 <saqsqe@gmail.com>
2026-02-17 06:13:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class Handler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(b"OK")
         else:
-            self.send_response(200)
+            self.send_response(500)
             self.end_headers()
             self.wfile.write(b"Service running")
 
