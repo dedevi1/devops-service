926ec43ea8abd1360c641ac3d26884b3b04a73ec
dedevi1 <saqsqe@gmail.com>
2026-02-18 10:14:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/health":
             if os.getenv("FAIL") == "1":
-            self.send_response(500)
+                self.send_response(500)
                 self.end_headers()
                 self.wfile.write(b"FAIL")
             else:
